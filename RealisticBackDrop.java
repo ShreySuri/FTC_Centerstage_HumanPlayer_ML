484bb47a5545bca92d1e0bbc4c1dd7911ca7533d
ShreySuri <shreysuri5@gmail.com>
2024-05-09 19:22:45 -0500

    working on realistic backdrop (rbd)

--- RealisticBackDrop.java
+++ RealisticBackDrop.java
@@ -9,10 +9,16 @@ public class RealisticBackDrop extends BackDrop {
         if (col < 0 || col >= 7) return;
     }
 
+    public void putPixel(Pixel p, int row, int col) {
+    } // This method should not be available for RealisticBackDrop
+
+
     private void knock(int row, int col, double strength) {
         // todo
         /*
         this method determines where the pixel at board[row][col] should go, based on the strength of what hits it
          */
     }
+
+
 }
